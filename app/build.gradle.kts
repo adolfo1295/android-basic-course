4f6abacda1c838be902e1fdd307e93da145d43eb
diegomedinapadilla <diemp94@gmail.com>
2024-01-16 16:09:06 -0700

    change target sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.moviesapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.moviesapp"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
