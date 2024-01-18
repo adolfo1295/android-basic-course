4eb67da67a877c28586c6408715697e8c99cffed
diegomedinapadilla <diemp94@gmail.com>
2024-01-17 22:42:13 -0700

    Adding Tabs to MainActivity

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -3,12 +3,27 @@ package com.example.moviesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moviesapp.ui.navigation.MoviesNavHost
 import com.example.moviesapp.ui.theme.MoviesAppTheme
@@ -16,6 +31,18 @@ import com.example.moviesapp.ui.theme.MoviesAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val tabItems = listOf(
+            TabItem(
+                title = "Home",
+                unselectedIcon = Icons.Outlined.Home,
+                selectedIcon = Icons.Filled.Home
+            ),
+            TabItem(
+                title = "Favorite",
+                unselectedIcon = Icons.Outlined.FavoriteBorder,
+                selectedIcon = Icons.Filled.Favorite
+            )
+        )
         setContent {
             MoviesAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,25 +50,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MoviesNavHost()
+                    var selectedTabIndex by remember {
+                        mutableIntStateOf(0)
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        TabRow(selectedTabIndex = selectedTabIndex) {
+                            tabItems.forEachIndexed { index, tabItem ->
+                                Tab(selected = index == selectedTabIndex
+                                    , onClick = {
+                                        selectedTabIndex = index
+                                },
+                                    text = {
+                                        Text(text = tabItem.title)
+                                    },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if(index == selectedTabIndex)
+                                                tabItem.selectedIcon else tabItem.unselectedIcon,
+                                            contentDescription = tabItem.title
+                                        )
+                                    }
+                                )
+                            }
+                        }
+                        when(selectedTabIndex){
+                            0 -> MoviesNavHost()
+                            1 -> Text(text = "FAVORITES")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class TabItem(
+    val title: String,
+    val unselectedIcon: ImageVector,
+    val selectedIcon: ImageVector
+)
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoviesAppTheme {
-        Greeting("Android")
+
     }
 }
